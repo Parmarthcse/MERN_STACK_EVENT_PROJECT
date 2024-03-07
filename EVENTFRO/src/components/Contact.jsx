c44ebc817b8d9d311dbbc56a9f0fd7a59231d4d0
Parmarthcse <gambhirsujal22@gmail.com>
2024-03-07 23:28:51 +0530

    last commit

--- EVENTFRO/src/components/Contact.jsx
+++ EVENTFRO/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://puce-vast-jellyfish.cyclic.app/api/v1/message/send",
         {
           name,
           email,
